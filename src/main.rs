d46e756cbabbd2366b9063380705115460f3b740
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-01-08 14:11:56 -0300

    creating GET /dinos

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,50 @@
-use tide::convert::Deserialize;
-use tide::convert::Serialize;
-#[derive(Debug, Deserialize, Serialize)]
+use async_std::sync::RwLock;
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::HashMap;
+use std::sync::Arc;
+use tide::{Body, Request, Response};
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
 struct Dino {
     name: String,
     weight: u16,
-    diet: String
+    diet: String,
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    dinos: Arc<RwLock<HashMap<String, Dino>>>,
 }
 
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
-    use tide::Request;
-    use tide::Response;
-    use tide::Body;
-    
     tide::log::start();
 
-    let mut app = tide::new();
-    
-    app.at("/").get(|_| async {
-        Ok("Hello, world!")
-    });
-    
-    app.at("/dinos").post(|mut req: Request<()>| async move {
+    let state = State {
+        dinos: Default::default(),
+    };
+
+    let mut app = tide::with_state(state);
+
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+
+    app.at("/dinos").post(|mut req: Request<State>| async move {
         let dino: Dino = req.body_json().await?;
-        println!("{:?}", dino);
+
+        let mut dinos = req.state().dinos.write().await;
+        dinos.insert(String::from(&dino.name), dino.clone());
         let mut res = Response::new(201);
         res.set_body(Body::from_json(&dino)?);
         Ok(res)
     });
-    
+
+    app.at("/dinos").get(|req: Request<State>| async move {
+        let dinos = req.state().dinos.read().await;
+        let dinos_vec: Vec<Dino> = dinos.values().cloned().collect();
+        let mut res = Response::new(200);
+        res.set_body(Body::from_json(&dinos_vec)?);
+        Ok(res)
+    });
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
